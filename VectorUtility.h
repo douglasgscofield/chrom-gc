fcc2be0e9c022327bde771a65d3512b3f1a36408
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-02-03 11:00:53 +0100

    Document VectorUtility.h with Doxygen-style comments, eventually I will move to using that

--- VectorUtility.h
+++ VectorUtility.h
@@ -18,8 +18,6 @@ namespace VectorUtility {
     template<class T> inline const T        Min(const std::vector<T>& Vec);
     template<class T> inline const double   Sum(const std::vector<T>& Vec);
     template<class T> inline const double   SumSquares(const std::vector<T>& Vec);
-    template<class T> inline void           Trim(std::vector<T>& Vec, 
-                                                 const T& val = ((T)0));
     template<class T> inline const double   Mean(const std::vector<T>& Vec);
     template<class T> inline const double   Var(const std::vector<T>& Vec, 
                                                 bool sample = true);
@@ -33,6 +31,12 @@ namespace VectorUtility {
     template<class T> inline std::map<T, std::vector<Scalar> >
                                             Runs(const std::vector<T>& Vec);
 
+    //! \brief Pairwise maximum for class T.
+    //!
+    //! \param  a       scalar of class T
+    //! \param  b       scalar of class T
+    //! \return         maximum of a and b, class T.
+    //!
     template<class T>
     inline const T
     Max(const T& a, const T& b)
@@ -40,14 +44,37 @@ namespace VectorUtility {
         return((a) > (b) ? (a) : (b));
     }
 
+    //! \brief Pairwise minimum for class T.
+    //!
+    //! \param  a       scalar of class T
+    //! \param  b       scalar of class T
+    //! \return         minimum of a and b, class T.
+    //!
+    template<class T>
+    inline const T
+    Min(const T& a, const T& b)
+    {
+        return((a) < (b) ? (a) : (b));
+    }
+
+    //! \brief Mean of values in vector of class T, returned as a double.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         mean of Vec, double.
+    //!
     template<class T>
     inline const double
     Mean(const std::vector<T>& Vec)
     {
         assert(Vec.size() > 0);
-        return(static_cast<double>(Sum(Vec))/static_cast<double>(Vec.size()));
+        return(Sum(Vec) / static_cast<double>(Vec.size()));
     }
 
+    //! \brief Sum of values in vector of class T, returned as a double.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         sum of Vec, double.
+    //!
     template<class T>
     inline const double
     Sum(const std::vector<T>& Vec)
@@ -59,6 +86,12 @@ namespace VectorUtility {
         return(ans);
     }
 
+    //! \brief Sum of squared values in vector of class T, returned as a 
+    //!        double.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         sum of squared values in Vec, double.
+    //!
     template<class T>
     inline const double
     SumSquares(const std::vector<T>& Vec)
@@ -71,6 +104,13 @@ namespace VectorUtility {
         return(ans);
     }
 
+    //! \brief Variance of values in vector of class T, returned as a double.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \param  sample  bool, calculates sample variance (SS/(n-1)) if true,
+    //!                 and population variance (SS/n) if false.
+    //! \return         variance of values in Vec, double.
+    //!
     template<class T>
     inline const double
     Var(const std::vector<T>& Vec, bool sample = true)
@@ -84,6 +124,11 @@ namespace VectorUtility {
         return(ans);
     }
 
+    //! \brief Maximum value in vector of class T.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         maximum in vector, class T.
+    //!
     template<class T>
     inline const T
     Max(const std::vector<T>& Vec)
@@ -95,13 +140,11 @@ namespace VectorUtility {
         return(ans);
     }
 
-    template<class T>
-    inline const T
-    Min(const T& a, const T& b)
-    {
-        return((a) < (b) ? (a) : (b));
-    }
-
+    //! \brief Minimum value in vector of class T.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         minimum in vector, class T.
+    //!
     template<class T>
     inline const T
     Min(const std::vector<T>& Vec) 
@@ -113,6 +156,11 @@ namespace VectorUtility {
         return(ans);
     }
 
+    //! \brief Pairwise absolute value for class T.
+    //!
+    //! \param  a       scalar of class T.
+    //! \return         absolute value of a, class T.
+    //!
     template<class T>
     inline const T
     Abs(const T a)
@@ -120,18 +168,12 @@ namespace VectorUtility {
         return((a) > ((T)0) ? (a) : -(a));
     }
 
-    template<class T>
-    inline void 
-    Trim(std::vector<T>& Vec, const T& val = ((T)0)) 
-    {
-        size_t trim = 0;
-        assert(Vec.size() > 0);
-        for (size_t i = Vec.size() - 1; i >= 0; --i) {
-            if (Vec[i] == val) { ++trim; } else { break; }
-        }
-        Vec.resize(Vec.size() - trim);
-    }
-
+    //! \brief Determine if value of class T is in vector of class T.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \param  val     value of class T.
+    //! \return         bool, true of val is in Vec
+    //!
     template<class T>
     inline const bool
     IsIn(const std::vector<T>& Vec, const T& val)
@@ -141,6 +183,12 @@ namespace VectorUtility {
         return(false);
     }
 
+    //! \brief Construct sequence of class T between two values.
+    //!
+    //! \param  from    start of sequence, class T.
+    //! \param  to      end of sequence, class T.
+    //! \return         vector of class T, sequence from .. to
+    //!
     template<class T>
     inline const std::vector<T>
     Seq(const T& from, const T& to)
@@ -155,6 +203,12 @@ namespace VectorUtility {
         return(ans);
     }
 
+    //! \brief Construct vector containing only unique values from vector 
+    //!        of class T.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         vector of class T, unique values from Vec
+    //!
     template<class T>
     inline const std::vector<T>
     Uniq(const std::vector<T>& Vec)
@@ -168,6 +222,18 @@ namespace VectorUtility {
         return(ans);
     }
 
+    //! \brief Construct map of runs of unique values in a vector,
+    //!        in order.
+    //!
+    //! A map of unique values in Vec, each mapping to a vector of
+    //! Scalar (long) values containing the run lengths of that value,
+    //! in order of appearance.  This is not a map of all runs in order,
+    //! that has yet to be constructed.
+    //!
+    //! \param  Vec     vector of class T.
+    //! \return         map<T, vector<Scalar> >, unique values from Vec
+    //!                 mapping to a vector of run lengths.
+    //!
     template<class T>
     inline std::map<T, std::vector<Scalar> >
     Runs(const std::vector<T>& Vec)
@@ -192,7 +258,7 @@ namespace VectorUtility {
     }
 
 
-    }  // namespace VectorUtility
+}  // namespace VectorUtility
 
 
 #endif // __VECTORUTILITY_H__
